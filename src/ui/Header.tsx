5385da066b6acf8203106822560be194da016ac6
empflow <empflow@tutanota.com>
2023-12-07 17:06:40 +0300

    add header

---
+++ src/ui/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="p-4 border-b border-gray-300">
+      <div className="max-w-[1000px] justify-between gap-2 m-auto w-full flex">
+        <h2>Recipes</h2>
+        <nav>Navigation Menu</nav>
+      </div>
+    </header>
+  );
+}
