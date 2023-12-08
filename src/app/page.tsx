3c557c0b7a40de9bf0a533541c9c812a0b279afc
empflow <empflow@tutanota.com>
2023-12-08 07:15:59 +0300

    add loading.tsx to /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import RecipesGrid from "@/components/home/RecipesGrid";
 
-export default function Home() {
+export default async function Home() {
   return (
     <>
       <h1 className="text-3xl font-semibold mb-8">Recipes</h1>
