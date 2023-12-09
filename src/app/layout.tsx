14b907c7a9bcbf63b2d9e173206c1b4c31cbea14
empflow <empflow@tutanota.com>
2023-12-09 08:39:30 +0300

    add <Providers> to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import MainContent from "@/components/MainContent";
+import Providers from "@/components/Providers";
 
 export const metadata: Metadata = {
   title: "Recipes App",
@@ -17,9 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
-        <Header />
-        <MainContent {...{ children }} />
-        <Footer />
+        <Providers>
+          <Header />
+          <MainContent {...{ children }} />
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
