2904d3710d8f7e164f5b68ec1559ad5ba0d68bbf
empflow <empflow@tutanota.com>
2023-12-10 20:02:38 +0300

    add super sick (though kinda complex) validation to addRecipeForm

--- src/lib/zod/schemas/addRecipeForm.ts
+++ src/lib/zod/schemas/addRecipeForm.ts
@@ -1,48 +1,58 @@
 import { z } from "zod";
 import checkFormNumber from "../checkFormNumber";
+import {
+  getInvalidTypeErrMsg,
+  getRequiredErrMsg,
+  getStrTooLongErrMsg,
+  getStrTooShortErrMsg,
+} from "../utils";
+import getFormNumSchema from "./formNum";
 
-const defaultMinLength = 3;
+const DEFAULT_MIN_LENGTH = 3;
 
 const TITLE_MAX_LENGTH = 300;
 const DESCRIPTION_MAX_LENGTH = 1500;
 
-const required_error = "This field is required";
-const tooShortErr = (fieldName: string, minLength: number) =>
-  `${fieldName} must be at least ${minLength} characters long`;
-const tooLongErr = (fieldName: string, maxLength: number) =>
-  `${fieldName} must not exceed ${maxLength} characters`;
+const FIELDS = {
+  title: "Title",
+  timeToCookMins: "Time to cook",
+  description: "Description",
+  isVegan: "Vegan",
+} as const;
 
-const addRecipeFormSchema = z.object({
+const AddRecipeFormSchema = z.object({
   title: z
-    .string({ invalid_type_error: "Title must be a string", required_error })
-    .min(defaultMinLength, { message: tooShortErr("Title", defaultMinLength) })
-    .max(TITLE_MAX_LENGTH, { message: tooLongErr("Title", TITLE_MAX_LENGTH) }),
-  timeToCookMins: z
-    .union([z.string(), z.number()])
-    .refine(checkFormNumber, "Time to cook is required")
-    .pipe(
-      z.coerce
-        .number()
-        .gt(0, { message: "Time to cook must be greater than zero" })
-    ),
+    .string({
+      invalid_type_error: getInvalidTypeErrMsg(FIELDS.title, "string"),
+      required_error: getRequiredErrMsg(FIELDS.title),
+    })
+    .min(1, { message: getRequiredErrMsg(FIELDS.title) })
+    .min(DEFAULT_MIN_LENGTH, {
+      message: getStrTooShortErrMsg(FIELDS.title, DEFAULT_MIN_LENGTH),
+    })
+    .max(TITLE_MAX_LENGTH, {
+      message: getStrTooLongErrMsg(FIELDS.title, TITLE_MAX_LENGTH),
+    }),
+  timeToCookMins: getFormNumSchema(FIELDS.timeToCookMins, { min: 1, max: 10 }),
   description: z
     .string({
-      invalid_type_error: "Description must be a string",
-      required_error,
+      invalid_type_error: getInvalidTypeErrMsg(FIELDS.description, "string"),
+      required_error: getRequiredErrMsg(FIELDS.description),
     })
-    .min(defaultMinLength, {
-      message: tooShortErr("Description", defaultMinLength),
+    .min(1, { message: getRequiredErrMsg(FIELDS.description) })
+    .min(DEFAULT_MIN_LENGTH, {
+      message: getStrTooShortErrMsg(FIELDS.description, DEFAULT_MIN_LENGTH),
     })
     .max(
       DESCRIPTION_MAX_LENGTH,
-      tooLongErr("Description", DESCRIPTION_MAX_LENGTH)
+      getStrTooLongErrMsg(FIELDS.description, DESCRIPTION_MAX_LENGTH)
     ),
   isVegan: z.boolean({
-    invalid_type_error: "Vegan must be a boolean",
-    required_error,
+    invalid_type_error: getInvalidTypeErrMsg(FIELDS.isVegan, "boolean"),
+    required_error: getRequiredErrMsg(FIELDS.isVegan),
   }),
 });
 
-export type AddRecipeFormSchema = z.infer<typeof addRecipeFormSchema>;
-export type AddRecipeFormSchemaInput = z.input<typeof addRecipeFormSchema>;
-export default addRecipeFormSchema;
+export type AddRecipeFormSchemaType = z.infer<typeof AddRecipeFormSchema>;
+export type AddRecipeFormSchemaInputType = z.input<typeof AddRecipeFormSchema>;
+export default AddRecipeFormSchema;
