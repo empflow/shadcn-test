36863eed6e85d9ea11d63568076458ddaeb53d75
empflow <empflow@tutanota.com>
2023-12-10 19:58:34 +0300

    add zod utils

---
+++ src/lib/zod/utils.ts
@@ -0,0 +1,20 @@
+import JsType from "../types/types";
+
+export const getRequiredErrMsg = (fieldName: string) =>
+  `${fieldName} is requried`;
+
+export const getNumTooSmallErrMsg = (fieldName: string, min: number) =>
+  `Minimum allowed ${fieldName.toLowerCase()} is ${min}`;
+export const getNumTooBigErrMsg = (fieldName: string, max: number) =>
+  `Maximum allowed ${fieldName.toLowerCase()} is ${max}`;
+
+export const getStrTooShortErrMsg = (fieldName: string, min: number) =>
+  `${fieldName} must be at least ${min} characters long`;
+export const getStrTooLongErrMsg = (fieldName: string, max: number) =>
+  `${fieldName} must not be longer than ${max} characters`;
+
+export const getInvalidTypeErrMsg = (fieldName: string, expectedType: JsType) =>
+  `${fieldName} must be a${expectedType[0] === "a" ? "n" : ""} ${expectedType}`;
+
+export const getInvalidNumberErrMsg = (fieldName: string) =>
+  `${fieldName} is not a valid number`;
