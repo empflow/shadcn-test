54369b32f6ddcb10f052e1e217676a4ec175e80b
empflow <empflow@tutanota.com>
2023-12-11 19:06:41 +0300

    underline site logo on hover

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,15 @@
+import UnderlinedLink from "./UnderlinedLink";
+
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="border-t border-gray-300 p-3 mt-16">
-      <div className="max-w-[1000px] m-auto">Recipes {year}</div>
+      <div className="max-w-[1000px] m-auto">
+        <UnderlinedLink underlineOnlyOnHover={true} href="/">
+          Recipes
+        </UnderlinedLink>{" "}
+        {year}
+      </div>
     </footer>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import UnderlinedLink from "./UnderlinedLink";
+
 export default function Header() {
   return (
     <header className="p-4 border-b border-gray-300">
       <div className="max-w-[1000px] justify-between gap-2 m-auto w-full flex">
-        <h2>Recipes</h2>
+        <UnderlinedLink underlineOnlyOnHover={true} href="/">
+          <h2>Recipes</h2>
+        </UnderlinedLink>
         <nav>Navigation Menu</nav>
       </div>
     </header>
